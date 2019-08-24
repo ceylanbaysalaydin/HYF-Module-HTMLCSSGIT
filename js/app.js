2ae863c68bdc278300d301a22a1187c097b92e56
ceylanbaysalaydin <ceylanbaysalaydin@gmail.com>
2019-08-25 01:56:34 +0200

    animation added

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 
-
 $(function () {
     $("#navbarNavDropdown ul li a.nav-link").click(function (e) { 
        // e.preventDefault();
@@ -8,8 +7,6 @@ $(function () {
         return true;
     });
 });
-
-
 function mySlideEffect(id,h3){
     $(function () {
         $(id+" "+ h3).click(function(e){
@@ -19,8 +16,70 @@ function mySlideEffect(id,h3){
     });
 }
 
+function bubbleAnimation(){
+    $(function () {
+        $("div.animation p").html("Bootstrap").css("color","white");
+        $("div.animation").animate({
+            top:"500px",
+            width:"155px",
+            height:"155px",
+            opacity:".6",
+        },2000,function (){
+            $("div.animation p").html("Css");
+        });
+       
+        $("div.animation").animate({
+            top:"400px",
+            width:"100px",
+            height:"100px",
+            right:"80px",
+            opacity:"1"
+        },2000,function (){
+            $("div.animation p").html("JS");
+        });
+        $("div.animation").animate({
+            top:"300px",
+            width:"80px",
+            height:"80px",
+            right:"40px",
+            opacity:".6"
+        },2000,function (){
+            $("div.animation p").html("Html");
+        });
+        $("div.animation").animate({
+            top:"200px",
+            width:"120px",
+            height:"120px",
+            right:"10px",
+            opacity:"1"
+        },2000,function (){
+            $("div.animation p").html("jQuery");
+        });
+        $("div.animation").animate({
+            top:"100px",
+            width:"100px",
+            height:"100px",
+            right:"80px",
+            opacity:".6"
+        },2000,function (){
+            $("div.animation p").html("Git");
+        });
+        $("div.animation").animate({
+            top:"0px",
+            width:"30px",
+            height:"30px",
+            right:"0px",
+            opacity:"0"
+        },2000);
+    
+    });
+}
+
 mySlideEffect("#education","h3");
 mySlideEffect("#hard-skills","h3");
 mySlideEffect("#soft-skills","h3");
 mySlideEffect("#certificates","h3");
+bubbleAnimation();
+
+
 
